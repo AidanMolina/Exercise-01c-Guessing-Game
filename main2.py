5c5db7d992a81ab6a3ab8f5d3454bdc01811b7c2
apmolina <apmolina@iu.edu>
2021-02-01 09:29:53 -0600

    Update main2.py
    Extra credit attempt

--- main2.py
+++ main2.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 import sys
+import random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+number = random.randrange(1,11)
+guess = input("Guess a number from 1 to 10: ")
+
+try:
+    guess = int(guess) 
+except ValueError:
+    print("That's not a number")
+    exit()
+
+if guess == number: 
+    print("Great job! You got it!")   
+elif guess != number: 
+    print("Sorry, better luck next time.") 
+    print("The number was " + str(number))
+
